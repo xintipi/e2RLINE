d450d35c08e7bd7c4cf8785dfa26980b9ecc7857
HUUTRUNG\Nguyen Huu Trung <huutrung.mmt@gmail.com>
2018-07-30 21:32:59 +0700

    responsive on ipad

--- app/js/main.js
+++ app/js/main.js
@@ -1,12 +1,5 @@
 $(document).ready(function () {
-  $("#panel-title_01, #panel-title_02, #panel-title_03").on("click", function () {
-    if ( $("#panel-title_01, #panel-title_02, #panel-title_03").hasClass("collapsed")) {
-      $(".glyphicon-plus").show()
-      $(".glyphicon-minus").hide()
-      if ($(this).hasClass("collapsed")) {
-        $(this).children(".glyphicon-plus").hide()
-        $(this).children(".glyphicon-minus").show()
-      }
-    }
-  })
+    $('.main-box').on('scroll', function () {
+        $('.thead', this).css('transform', 'translateY(' + this.scrollTop + 'px)')
+    })
 })
